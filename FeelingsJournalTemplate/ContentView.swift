051761acee4790e1ed1dd690920280c3e8379167
Zoe Cutler <zcutlerf@gmail.com>
2023-02-07 11:44:32 -0500

    Initial Commit

---
+++ FeelingsJournalTemplate/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FeelingsJournalTemplate
+//
+//  Created by Zoe Cutler on 2/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FeelingsJournalTemplate/FeelingsJournalTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeelingsJournalTemplateApp.swift
+//  FeelingsJournalTemplate
+//
+//  Created by Zoe Cutler on 2/7/23.
+//
+
+import SwiftUI
+
+@main
+struct FeelingsJournalTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
